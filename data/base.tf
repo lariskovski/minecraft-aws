ba18fc100448fd97ce4e228f2cbfcafe7db1fbbe
lariskovski <larissaporto@live.com>
2022-06-15 01:59:29 -0300

    refactoring: Merge networking into data dir

--- data/base.tf
+++ data/base.tf
@@ -76,9 +76,9 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role" "this" {
-  name = "SSMInstanceProfile"
+  name               = "SSMInstanceProfile"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-  
+
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
 }
 
--- data/variables.tf
+++ data/variables.tf
@@ -1,3 +1,8 @@
+
+variable "project_name" {
+  type = string
+}
+
 variable "region" {
   type = string
 }
