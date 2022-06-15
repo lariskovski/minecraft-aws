1ff06e2296e670e2bd32b66966fa6c4004eb3d8d
lariskovski <larissaporto@live.com>
2022-06-15 03:12:20 -0300

    Fix env.sh PKR_VAR_region

--- env.sh
+++ env.sh
@@ -7,11 +7,6 @@ export TF_VAR_availability_zone_name="us-east-1c"
 export TF_VAR_instance_type="t2.small"
 
 export PKR_VAR_project_name=$TF_VAR_project_name
-export PKR_VAR_region=$TF_VAR_project_name
+export PKR_VAR_region=$TF_VAR_region
 
-
-
-##################################
-# exported after networking is ok
-##################################
 
